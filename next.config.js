0c68add4a6f57412b9fcc9d17ff15fe3cc022096
Kevin Holm <holmk21@gmail.com>
2025-06-04 21:27:15 +0100

    fixing form completion

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  trailingSlash: true, // Required for static routing to work on Netlify
-  images: {
-    unoptimized: true, // Needed if you're using <Image> but not optimizing on server
-  },
 };
 
 module.exports = nextConfig;
-
