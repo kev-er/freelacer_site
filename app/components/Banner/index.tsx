b4ee508632014e2d19c967dca6cb3fce5df028d4
Kevin Holm <holmk21@gmail.com>
2025-06-05 10:42:21 +0100

    ...

--- app/components/Banner/index.tsx
+++ app/components/Banner/index.tsx
@@ -1,38 +1,38 @@
 import Image from "next/image";
 
+// MAIN BANNER COMPONENT
 const Banner = () => {
-    return (
-        <div className='mx-auto max-w-7xl my-10 sm:py-10 px-6 lg:px-8'>
-            <div className='grid grid-cols-1 lg:grid-cols-2 my-16'>
+  return (
+    <div className='mx-auto max-w-7xl my-10 sm:py-10 px-6 lg:px-8'>
+      <div className='grid grid-cols-1 lg:grid-cols-2 my-16'>
 
-                {/* COLUMN-1 */}
-
-                <div className="mx-auto sm:mx-0">
-                    <div className='py-3 text-center lg:text-start'>
-                        <button className='text-blue bg-lightblue hover:shadow-xl text-sm md:text-lg font-bold px-6 py-1 rounded-3xl tracking-wider hover:text-white hover:bg-black'>FREELANCER</button>
-                    </div>
-                    <div className="py-3 text-center lg:text-start">
-                        <h1 className='text-6xl lg:text-80xl font-bold text-darkpurple'>
-                            Dedicated to <br /> bringing your <br /> ideas to life.
-                        </h1>
-                    </div>
-                    <div className='my-7 text-center lg:text-start'>
-                    </div>
-                </div>
-
-                {/* COLUMN-2 */}
-
-                <div className='lg:-m-24 lg:pt-20 hidden lg:block'>
-                    <Image src="/images/banner/banner.png" alt="hero-image" width={800} height={642} />
-                </div>
-
-            </div>
+        {/* COLUMN-1 */}
+        <div className="mx-auto sm:mx-0">
+          <div className='py-3 text-center lg:text-start'>
+            <button className='text-blue bg-lightblue hover:shadow-xl text-sm md:text-lg font-bold px-6 py-1 rounded-3xl tracking-wider hover:text-white hover:bg-black'>
+              FREELANCER
+            </button>
+          </div>
+          <div className="py-3 text-center lg:text-start">
+            <h1 className='text-6xl lg:text-80xl font-bold text-darkpurple'>
+              Dedicated to <br /> bringing your <br /> ideas to life.
+            </h1>
+          </div>
+          <div className='my-7 text-center lg:text-start'></div>
         </div>
-    )
-}
 
+        {/* COLUMN-2 */}
+        <div className='lg:-m-24 lg:pt-20 hidden lg:block'>
+          <Image src="/images/banner/banner.png" alt="hero-image" width={800} height={642} />
+        </div>
 
-export default function HiddenNetlifyForm() {
+      </div>
+    </div>
+  );
+};
+
+// CHANGED to named export
+export function HiddenNetlifyForm() {
   return (
     <form
       name="contact"
@@ -48,5 +48,6 @@ export default function HiddenNetlifyForm() {
   );
 }
 
-
+// Only one default export allowed
 export default Banner;
+
