588ead4fb95692fedc741e7b251935a10d34864f
Kevin Holm <holmk21@gmail.com>
2025-06-09 22:46:28 +0100

    adding thank-you page

---
+++ app/components/thank-you/index.tsx
@@ -0,0 +1,7 @@
+export default function ThankYou() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-3xl font-bold">Thank you for contacting us!</h1>
+    </div>
+  );
+}
